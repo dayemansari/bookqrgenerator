2cea2ccd25a0ef19ac2452ad290165d98e6c45ca
Mohammad Dayem Ansari <103567448+dayemansari@users.noreply.github.com>
2023-05-21 12:55:50 +0530

    Create table_structure.sql
    SQL Table Structure for the Respective Python Code

---
+++ table_structure.sql
@@ -0,0 +1,9 @@
+CREATE TABLE DETAILS (
+    Owner VARCHAR(99) NULL,
+    Date_Purchased DATE NULL,
+    Book_Name VARCHAR(99) NULL,
+    Author VARCHAR(500) NULL,
+    Publisher VARCHAR(500) NULL,
+    Code VARCHAR(500) NULL,
+    Date_Finished VARCHAR(50) NULL
+);
